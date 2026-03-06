8c4b8b176d84a1aa7eee5e460692a945e746c7db
Bernd Schuller <b.schuller@fz-juelich.de>
2026-03-06 13:54:25 +0100

    update jline and groovy dependencies

--- core/src/test/resources/groovy/test.groovy
+++ core/src/test/resources/groovy/test.groovy
@@ -1,20 +1,28 @@
 /*
 * for testing the Groovy script facility
 */
+println "*** Groovy test script."
+println ""
+println "The following variables are available:"
+println ""
+binding.variables.each {
+  println it.key +": " + it.value.getClass().name
+}
+println ""
 
-println "This is a test Groovy script"
-
-//check that the context for Groovy scripts is setup correctly
-assert(registry!=null)
-assert(configurationProvider!=null)
-assert(registryURL!=null)
-assert(properties!=null)
-assert(options!=null)
-assert(commandLine!=null)
+// check that the context for Groovy scripts is setup as expected
+assert auth!=null
+assert configurationProvider!=null
+assert registry!=null
+assert registryURL!=null
+assert properties!=null
+assert options!=null
+assert commandLine!=null
 
 // example from UCC manual
 
-// list storages from registry
-registry.listEntries("StorageManagement").each { 
-    messageWriter.message("Storage at " + it.url)			    
+// list core service endpoints from registry
+registry.listEntries("CoreServices").each {
+    console.info("Core API endpoint at: {}", it.url)
 }
+println "*** END of Groovy test"
